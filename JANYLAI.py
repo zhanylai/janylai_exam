de9bfdc3e6144c6487ac98911c4377fd3e73cc2e
kenjan <kenjekulov66@gmail.com>
2018-12-19 09:37:34 +0600

    new

---
+++ JANYLAI.py
@@ -0,0 +1,5 @@
+def find_list_middle(a):
+    mid = len(a)/2+1
+    return a[mid]
+my_group=[saddam,kasiet,jarkynai,bermet,bayel1,azamat1,daniyar,adahan,maksat,bekbolot,azamat2,avtandil,bekbolot2,aizat,kadyr]
+
